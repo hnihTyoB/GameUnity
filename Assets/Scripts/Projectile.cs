f95421d0e9441d1d09c193dee0d5c8ebcd53d4fd
hnihTyoB <nctmdt@gmail.com>
2025-10-03 10:59:00 +0700

    Part 28 11:00

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,9 +5,46 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 22f;
+    [SerializeField] private GameObject particleOnHitPrefabVFX;
+    private WeaponInfo weaponInfo;
+    private Vector3 startPosition;
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
     private void Update()
     {
         MoveProjectile();
+        DetectFireDistance();
+    }
+    public void UpdateWeaponInfo(WeaponInfo weaponInfo){
+        this.weaponInfo = weaponInfo;
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        EnemyHealth enemyHealth = other.gameObject.GetComponent<EnemyHealth>();
+        Indestructible indestructible = other.gameObject.GetComponent<Indestructible>();
+        // PlayerHealth player = other.gameObject.GetComponent<PlayerHealth>();
+
+        // if (!other.isTrigger && (enemyHealth || indestructible || player)) {
+        //     if ((player && isEnemyProjectile) || (enemyHealth && !isEnemyProjectile)) {
+        //         player?.TakeDamage(1, transform);
+        //         Instantiate(particleOnHitPrefabVFX, transform.position, transform.rotation);
+        //         Destroy(gameObject);
+        //     } else if (!other.isTrigger && indestructible) {
+        //         Instantiate(particleOnHitPrefabVFX, transform.position, transform.rotation);
+        //         Destroy(gameObject);
+        //     }
+        // }
+        if (!other.isTrigger && (enemyHealth || indestructible)) {
+            Instantiate(particleOnHitPrefabVFX, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+    private void DetectFireDistance() {
+        if (Vector3.Distance(transform.position, startPosition) > weaponInfo.weaponRange) {
+            Destroy(gameObject);
+        }
     }
     private void MoveProjectile()
     {
--- Assets/Scripts/UI/Staff.cs
+++ Assets/Scripts/UI/Staff.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class Staff : MonoBehaviour, IWeapon
 {
     [SerializeField] private WeaponInfo weaponInfo;
+    [SerializeField] private GameObject magicLaser;
+    [SerializeField] private Transform magicLaserSpawnPoint;
+
+    private Animator myAnimator;
+
+    readonly int ATTACK_HASH = Animator.StringToHash("Attack");
+    private void Awake() {
+        myAnimator = GetComponent<Animator>();
+    }
     private void Update()
     {
         MouseFollowWithOffset();
@@ -15,7 +24,16 @@ public class Staff : MonoBehaviour, IWeapon
         // myAnimator.SetTrigger(FIRE_HASH);
         // GameObject newArrow = Instantiate(arrowPrefab, arrowSpawnPoint.position, ActiveWeapon.Instance.transform.rotation);
         // newArrow.GetComponent<Projectile>().UpdateProjectileRange(weaponInfo.weaponRange);
-        Debug.Log("Staff Attack");
+        myAnimator.SetTrigger(ATTACK_HASH);
+    }
+
+    public void SpawnStaffProjectileAnimEvent() {
+        GameObject newLaser = Instantiate(magicLaser, magicLaserSpawnPoint.position, Quaternion.identity);
+        // newLaser.GetComponent<MagicLaser>().UpdateLaserRange(weaponInfo.weaponRange);
+    }
+    public WeaponInfo GetWeaponInfo()
+    {
+        return weaponInfo;
     }
 
     private void MouseFollowWithOffset()
@@ -35,8 +53,4 @@ public class Staff : MonoBehaviour, IWeapon
         }
     }
     
-    public WeaponInfo GetWeaponInfo()
-    {
-        return weaponInfo;
-    }
 }
