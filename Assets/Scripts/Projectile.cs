6667354373574e542f2927ee2cf589e6e143a6c9
hnihTyoB <nctmdt@gmail.com>
2025-10-07 12:09:52 +0700

    Part 35 12:09

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -21,18 +21,25 @@ public class Projectile : MonoBehaviour
     public void UpdateProjectileRange(float projectileRange){
         this.projectileRange = projectileRange;
     }
+    public void UpdateMoveSpeed(float moveSpeed){
+        this.moveSpeed = moveSpeed;
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         EnemyHealth enemyHealth = other.gameObject.GetComponent<EnemyHealth>();
         Indestructible indestructible = other.gameObject.GetComponent<Indestructible>();
         PlayerHealth player = other.gameObject.GetComponent<PlayerHealth>();
 
-        if (!other.isTrigger && (enemyHealth || indestructible || player)) {
-            if ((player && isEnemyProjectile) || (enemyHealth && !isEnemyProjectile)) {
+        if (!other.isTrigger && (enemyHealth || indestructible || player))
+        {
+            if ((player && isEnemyProjectile) || (enemyHealth && !isEnemyProjectile))
+            {
                 player?.TakeDamage(1, transform);
                 Instantiate(particleOnHitPrefabVFX, transform.position, transform.rotation);
                 Destroy(gameObject);
-            } else if (!other.isTrigger && indestructible) {
+            }
+            else if (!other.isTrigger && indestructible)
+            {
                 Instantiate(particleOnHitPrefabVFX, transform.position, transform.rotation);
                 Destroy(gameObject);
             }
