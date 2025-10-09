e7982986a4534538b1e824c4be02eba8a0ff49f6
hnihTyoB <nctmdt@gmail.com>
2025-10-09 08:55:27 +0700

    Part 38 08:55

---
+++ Assets/Scripts/PickUpSpawner.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class PickUpSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject goldCoinPrefab;
+
+    public void DropItems()
+    {
+        Instantiate(goldCoinPrefab, transform.position, Quaternion.identity);
+    }
+}
